80649efdf7b8bcb835c53da1be5cdd148d365d7b
Thiago <thiago14lost@gmail.com>
2023-01-25 16:07:31 -0300

    doc: create doc.go

---
+++ pswdvalidation/doc.go
@@ -0,0 +1,12 @@
+/*
+This module contains all password validation operations.
+
+model.go: definition of data transfer objects.
+
+routers.go: router binding
+
+controllers.go: core logic
+
+services.go: auxiliary functions used on controllers.
+*/
+package pswdvalidation
